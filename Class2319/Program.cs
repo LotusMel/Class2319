c72d99fb4d3f63f12f48d060e39430cc49affde9
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-02-03 20:14:00 -0600

    Add project files.

---
+++ Class2319/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class2319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
